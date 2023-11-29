e9ff40c338a0b047626cde7d512827918043ec45
Jordan Chow <151275675+OFFICIALghoul@users.noreply.github.com>
2023-11-29 13:16:19 +1100

    Made encryption more advanced

--- OfficialGhoul/js/main.js
+++ OfficialGhoul/js/main.js
@@ -1,25 +1,32 @@
 
-function randomizeCasing() {
-    
+function randomizeCasing(letter) {
+    const trueOrFalse = Math.random();
+    if(trueOrFalse > 0.5){
+        console.log('true')
+        return letter.toUpperCase();
+    } else{
+        console.log('false')
+        return letter.toLowerCase();
+    }
 }
 
 function encrypt(plainString, n) {
     let encryptString = '';
-    const tempString = plainString;
-
+    let tempValue = ''
+    const tempString = plainString.toUpperCase();
     for (let i = 0; i < tempString.length; i++) {
         let charNum = tempString.charCodeAt(i);
-
         if (charNum <= 90 && charNum >= 65) {
             charNum += n;
             if (charNum > 90) {
                 charNum -= 26;
             }
         }
-        encryptString += String.fromCharCode(charNum);
+        tempValue = String.fromCharCode(charNum)
+        console.log('TempValue = '+ tempValue)
+        encryptString += randomizeCasing(tempValue);
     }
-    encryptString += ' '
-    encryptString += ranValues()
+    encryptString += ranValues();
     return encryptString;
 }
 
@@ -46,10 +53,12 @@ function decrypt(encryptString, n) {
 function ranValues() {
     let ranValue = '';
     let length = 5;
+    let tempValue = '';
 
     for (let i = 0; i < length; i++) {
         let charNum = Math.floor(Math.random() * 26) + 65;
-        ranValue += String.fromCharCode(charNum);
+        tempValue = String.fromCharCode(charNum)
+        ranValue += randomizeCasing(tempValue);
     }
     return ranValue;
 }
@@ -62,7 +71,6 @@ function printEncryption() {
 
     if (encryptionValue && number) {
         const shiftNumber = parseInt(number, 10);
-
         resultElement.innerHTML = encrypt(encryptionValue, shiftNumber);
     } else {
         resultElement.innerHTML = 'Please fill in both fields.';
@@ -75,28 +83,18 @@ function printDecryption() {
     const number = document.getElementById('reverseNumber').value;
     const resultElement = document.getElementById('decryptResult');
 
-
     if (encryptionValue && number) {
         const shiftNumber = parseInt(number, 10);
-
         resultElement.innerHTML = decrypt(encryptionValue, shiftNumber);
     } else {
         resultElement.innerHTML = 'Please fill in both fields.';
     }
 }
 
-document.querySelector('.leftSide').addEventListener('mouseover', openMenu);
-document.querySelector('.leftSide').addEventListener('mouseout', closeMenu);
+console.log(encrypt('Hello', 3))
+const leftSide = document.querySelector('.leftSide');
 
-function closeMenu() {
-    const list = document.getElementById('list');
-    const menu = document.getElementsByClassName('menu')[0];
-    const leftSide = document.getElementsByClassName('leftSide')[0];
-    list.style.display = 'none';
-    menu.classList.remove('active');
-    leftSide.classList.remove('active');
-}
-
+leftSide.addEventListener('mouseover', openMenu);
 
 function openMenu() {
     const list = document.getElementById('list');
@@ -105,6 +103,17 @@ function openMenu() {
     list.style.display = 'block';
     menu.classList.add('active');
     leftSide.classList.add('active');
+}
+
+leftSide.addEventListener('mouseout', closeMenu);
+
+function closeMenu() {
+    const list = document.getElementById('list');
+    const menu = document.getElementsByClassName('menu')[0];
+    const leftSide = document.getElementsByClassName('leftSide')[0];
+    list.style.display = 'none';
+    menu.classList.remove('active');
+    leftSide.classList.remove('active');
 }
 
 window.addEventListener('scroll', function () {
